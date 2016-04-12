fc554d9b59433c239286c67d8450378f00bb7952
luissancar <luissancar@yahoo.es>
2016-04-12 18:04:18 +0200

    modification 2

--- app/src/main/java/com/cep/cursoandroid/calculadora2016/MainActivity.java
+++ app/src/main/java/com/cep/cursoandroid/calculadora2016/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void operar() {
-        switch (charBoton) {
+        switch (charBoton) { 
             case ('+'):
                 res += resultadoDisplay();
                 break;
